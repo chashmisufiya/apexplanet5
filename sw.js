bc47f9b49e5d6ca0c51e8be308fbc60510727747
chashmisufiya <sdchashmisufiya086@gmail.com>
2025-09-14 20:56:11 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+self.addEventListener("install",e=>{e.waitUntil(caches.open("swiftcart-v1").then(c=>c.addAll(["/","/index.html","/cart.html","/wishlist.html","/checkout.html","/style.min.css","/app.min.js"])))});self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
